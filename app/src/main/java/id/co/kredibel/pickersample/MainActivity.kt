a04ab484340844a6030481766274bcdf3df56f29
Hangga Aji Sayekti <bazeniancode@gmail.com>
2022-06-01 14:17:11 +0700

    [Update] 0.0.4

--- app/src/main/java/id/co/kredibel/pickersample/MainActivity.kt
+++ app/src/main/java/id/co/kredibel/pickersample/MainActivity.kt
@@ -7,28 +7,21 @@ import androidx.appcompat.app.AppCompatActivity
 import io.kredibel.picker.Picker
 
 class MainActivity : AppCompatActivity() {
+
     var picker = Picker(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val imgResult = findViewById<ImageView>(R.id.imgResult)
         findViewById<View>(R.id.btnCamera).setOnClickListener {
-
-            /*picker.pickCamera { uri: Uri?, file: File?, bitmap: Bitmap? ->
-                imgResult.setImageURI(uri)
-            }*/
-
             picker.pickCamera { uri, file, bitmap ->
                 imgResult.setImageURI(uri)
             }
-
         }
         findViewById<View>(R.id.btnGalery).setOnClickListener {
-
             picker.pickGallery { uri, file, bitmap ->
                 imgResult.setImageURI(uri)
             }
-
         }
     }
 }
